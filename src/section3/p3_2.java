1b0dbd0d0d90be3175f2c87398bf3241cfb3941a
shinyehwan <jghisjl007@naver.com>
2022-12-07 22:28:36 +0900

    3-2

--- src/section3/p3_2.java
+++ src/section3/p3_2.java
@@ -11,14 +11,25 @@ public class p3_2 {
 		int k = sc.nextInt();
 
 		int[] data = new int[n];
+
 		for (int i = 0; i < n; i++) {
 			data[i] = sc.nextInt();
 		}
 
 		Arrays.sort(data);
 
-		int sum = data[0] * k;
-		sum += data[1] * k;
+
+		// 수열을 찾아내는 것이 중요하다
+		int cnt = (m / (k + 1)) * k;
+		cnt += m % (k + 1);
+
+		int first = data[n - 1];
+		int second = data[n - 2];
+
+		int result = first * cnt;
+		result += second * (m - cnt);
+
+		System.out.println(result);
 
 	}
 }
